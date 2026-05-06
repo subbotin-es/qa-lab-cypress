479d818f065db67fde996b2e56b69910ca4a4277
Evgenii Subbotin <avguard@gmail.com>
2026-05-06 21:52:24 +0200

    Fix CI test step

--- cypress/support/page-objects/forms.js
+++ cypress/support/page-objects/forms.js
@@ -12,13 +12,13 @@ const FormsPage = {
 
   fillForm({ fullName, email, age, phone }) {
     cy.get(this.selectors.fullNameInput).clear();
-    cy.get(this.selectors.fullNameInput).type(fullName);
+    if (fullName) cy.get(this.selectors.fullNameInput).type(fullName);
     cy.get(this.selectors.emailInput).clear();
-    cy.get(this.selectors.emailInput).type(email);
+    if (email) cy.get(this.selectors.emailInput).type(email);
     cy.get(this.selectors.ageInput).clear();
-    cy.get(this.selectors.ageInput).type(String(age));
+    if (age !== undefined && age !== null && age !== '') cy.get(this.selectors.ageInput).type(String(age));
     cy.get(this.selectors.phoneInput).clear();
-    cy.get(this.selectors.phoneInput).type(phone);
+    if (phone) cy.get(this.selectors.phoneInput).type(phone);
   },
 
   submit() {
